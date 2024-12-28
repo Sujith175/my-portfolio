1aa825ee476f79761deccb4c004894f043087817
Sujith Kumar R <sujithkumar.sk175@gmail.com>
2024-12-29 00:37:22 +0530

    fixed

---
+++ src/assets/index.js
@@ -0,0 +1,65 @@
+import logo from "./logo.svg";
+
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import view from "./view.png";
+
+import css from "./tech/css.png";
+import docker from "./tech/docker.png";
+
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import mentorow from "./company/mentorow.png";
+import cyra from "./company/cyra.png";
+import EC2 from "./tech/EC2.png";
+import school from "./company/school.png";
+import redhat from "./company/redhat.png";
+
+import fitness from "./fitness_screen.png";
+import crown from "./crown.png";
+import cosmetics from "./cosmetics.png";
+import dalle from "./dall e.png";
+import node from "./node.png";
+import Mern from "./tech/MERN-logo.png";
+import ecom from "./company/ecom.png";
+// import mobile from "./mobile.png";
+export {
+  logo,
+  school,
+  // mobile,
+  ecom,
+  github,
+  menu,
+  close,
+  css,
+  docker,
+  git,
+  html,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  mentorow,
+  tailwind,
+  typescript,
+  redhat,
+  fitness,
+  crown,
+  cosmetics,
+  dalle,
+  view,
+  cyra,
+  EC2,
+  node,
+  Mern,
+};
---
+++ src/components/About.jsx
@@ -0,0 +1,72 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className="xs:w-[250px] w-full">
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+      >
+        <img
+          src={icon}
+          alt="web-development"
+          className="w-16 h-16 object-contain"
+        />
+
+        <h3 className="text-white text-[20px] font-bold text-center">
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        Hello and welcome to my portfolio! My name is Sujith Kumar R, and I am
+        an experienced Full Stack Developer with a passion for building scalable
+        and efficient web applications. Over the years, I have honed my skills
+        in JavaScript, React.js, and the MERN stack, along with expertise in
+        deployment technologies like Docker containers and AWS EC2. My portfolio
+        highlights a range of projects that reflect my technical proficiency,
+        problem-solving capabilities, and commitment to delivering high-quality
+        solutions. I am enthusiastic about collaborating with dynamic teams to
+        create impactful digital experiences. Thank you for visiting my
+        portfolio, and I look forward to connecting with you and contributing to
+        your next big project!
+      </motion.p>
+
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
